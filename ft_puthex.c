752b71474aaa543b1cfad11452c2756d8a1ed9b4
Tomas Petros <tpetros@lab1r3s3.42abudhabi.ae>
2023-01-14 19:16:20 +0400

    ft_puthex optimized

--- ft_puthex.c
+++ ft_puthex.c
@@ -12,45 +12,39 @@
 
 #include "ft_printf.h"
 
-static int	ft_hex_len(unsigned	int num)
+static int	ft_put_hex(unsigned int num, const char format)
 {
-	int	len;
+	int	count;
 
-	len = 0;
-	while (num != 0)
-	{
-		len++;
-		num = num / 16;
-	}
-	return (len);
-}
-
-static void	ft_put_hex(unsigned int num, const char format)
-{
+	count = 0;
 	if (num >= 16)
 	{
-		ft_put_hex(num / 16, format);
-		ft_put_hex(num % 16, format);
+		count += ft_put_hex(num / 16, format);
+		count += ft_put_hex(num % 16, format);
 	}
 	else
 	{
 		if (num <= 9)
-			ft_putchar((num + '0'));
+			count += ft_putchar((num + '0'));
 		else
 		{
 			if (format == 'x')
-				ft_putchar((num - 10 + 'a'));
+				count += ft_putchar((num - 10 + 'a'));
 			if (format == 'X')
-				ft_putchar((num - 10 + 'A'));
+				count += ft_putchar((num - 10 + 'A'));
 		}
 	}
+	return (count);
 }
 
 int	ft_puthex(unsigned int num, const char format)
 {
+	int	count;
+
+	count = 0;
 	if (num == 0)
 		return (ft_putchar((num + '0')));
 	else
-		ft_put_hex(num, format);
-	return (ft_hex_len(num));
+		count += ft_put_hex(num, format);
+	return (count);
 }
