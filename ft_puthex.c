7e7e48b73235acfbbb7e8fa4df857a51d94ab8a3
Tomas Petros <tpetros@lab1r3s3.42abudhabi.ae>
2023-01-14 18:54:59 +0400

    First printf commit

---
+++ ft_puthex.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_puthex.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tpetros <tpetros@student.42abudhabi.ae>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/10 20:16:16 by tpetros           #+#    #+#             */
+/*   Updated: 2023/01/10 20:16:17 by tpetros          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+
+static int	ft_hex_len(unsigned	int num)
+{
+	int	len;
+
+	len = 0;
+	while (num != 0)
+	{
+		len++;
+		num = num / 16;
+	}
+	return (len);
+}
+
+static void	ft_put_hex(unsigned int num, const char format)
+{
+	if (num >= 16)
+	{
+		ft_put_hex(num / 16, format);
+		ft_put_hex(num % 16, format);
+	}
+	else
+	{
+		if (num <= 9)
+			ft_putchar((num + '0'));
+		else
+		{
+			if (format == 'x')
+				ft_putchar((num - 10 + 'a'));
+			if (format == 'X')
+				ft_putchar((num - 10 + 'A'));
+		}
+	}
+}
+
+int	ft_puthex(unsigned int num, const char format)
+{
+	if (num == 0)
+		return (ft_putchar((num + '0')));
+	else
+		ft_put_hex(num, format);
+	return (ft_hex_len(num));
+}
